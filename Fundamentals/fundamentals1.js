6a54e71183403f91b342f3f0e0d10476a0ceb8a0
Alexsander Marchi Zunino <alexsandermarchi@gmail.com>
2024-05-05 23:59:00 -0300

    Conditional (ternary) Operator

--- Fundamentals/fundamentals1.js
+++ Fundamentals/fundamentals1.js
@@ -79,3 +79,28 @@ if (scoreDolphins > scoreKoalas){
 }else{
     console.log("Both win the trophy")
 }
+
+
+//expression is what produce a value
+
+//The Conditional (ternary) Operator
+const age = 23;
+age >= 18 ? console.log("You can drive") : console.log("Can't drive"); //three parts: condition, if part (?) and else part (:)
+
+
+const drink = age >= 18 ? "yes, Wine" : "no, only Water" //transfprm in an expression
+console.log(drink)
+
+/*
+CHALLENGE #4
+Steven needs a very simple tip calculator for whenever he goes to eat in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+Your tasks:
+
+Calculate the tip, depending on the bill value. Create a variable called tip for this. It's not allowed to use an if...else statement (if it's easier for you, you can start with an if...else statement, and then try to convert it to a ternary operator).
+
+Print a string to the console containing the bill value, the tip, and the final value (bill + tip).*/
+const bill = 275;
+let tip = bill >= 50 && bill <=300 ? bill * 0.15 : bill * 0.20;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill+tip}`);
