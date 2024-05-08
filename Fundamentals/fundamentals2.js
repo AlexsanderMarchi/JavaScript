67803ff7c18ee9578c5fccd599bfe4753b1dd948
Alexsander Marchi Zunino <alexsandermarchi@gmail.com>
2024-05-08 00:44:23 -0300

    Exercises

--- Fundamentals/fundamentals2.js
+++ Fundamentals/fundamentals2.js
@@ -43,15 +43,74 @@ const age2 = calcAge2(1991)
 
 //Function expression (Arrow Function)
  const calcAge3 = birthYear => 2037 - birthYear;
-const age3 = calcAge3(1991);
+    const age3 = calcAge3(1991);
 
  console.log(age, age2, age3);
 
  const yearsUntilRetirement = (birthYear, firstName) => {
-    const age = 2037 - birthYear;
+    const age = calcAge3(birthYear);
     const retirement = 65 - age;
+
+    if(retirement > 0){
+        console.log(`${firstName} retires in ${retirement} years`)
+        return retirement;
+    }else{
+        console.log(`${firstName} has already retired`)
+        return -1;
+    }
+
     // return retirement;
-    return `${firstName} retires in ${retirement} years`
+    // return `${firstName} retires in ${retirement} years`
  }
 
  console.log(yearsUntilRetirement(1991, 'Jonas'));
+ console.log(yearsUntilRetirement(1950, 'Jonas2'));
+
+ /*
+ CHALLENGE #1
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
+
+
+
+Your tasks:
+
+Create an arrow function calcAverage to calculate the average of 3 scores. This function should have three parameters and return a single number (the average score).
+Create two new variables — scoreDolphins and scoreKoalas, and assign the value returned from the calcAverage function to them (you will need to call this function, and pass scores as arguments).
+Create a function checkWinner that takes the average score of each team as parameters (avgDolphins and avgKoalas), and then logs the winner to the console, together with the victory points, according to the rule above. Example: Koalas win (30 vs. 13) (use avgDolphins and avgKoalas instead of hard-coded values).
+Use the checkWinner function to determine the winner for both DATA 1 and DATA 2.
+Ignore draws this time. Instead, log No team wins... to the console if there is no winner.
+
+
+
+TEST DATA 1: Dolphins scored 44, 23, and 71. Koalas scored 65, 54, and 49.
+TEST DATA 2: Dolphins scored 85, 54, and 41. Koalas scored 23, 34, and 27. 
+
+*/
+const calcAverage = (num1, num2, num3) => {
+    const averageScore = (num1+num2+num3)/3 ;
+    return averageScore; 
+};
+
+function checkWinner (avgDolphins, avgKoalas){
+    if(avgDolphins >= avgKoalas*2){
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`)
+    }else if(avgKoalas >= avgDolphins*2){
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`)
+    }else{
+        console.log('No team wins...')
+    }
+    
+}
+
+//Data 1
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
+
+//Data 2
+const scoreDolphins2 = calcAverage(85, 54, 41);
+const scoreKoalas2 = calcAverage(23, 34, 27);
+
+checkWinner(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins2, scoreKoalas2);
